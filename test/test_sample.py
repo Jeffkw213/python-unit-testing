c43f69dd503c4dd7c6af8c209d7075e66d491d5a
Jeff <jeffkwan10@gmail.com>
2026-04-04 22:36:29 -0400

    first commit

---
+++ test/test_sample.py
@@ -0,0 +1,12 @@
+from app.calculator import add, subtract, multiply, divide
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 3) == 2
